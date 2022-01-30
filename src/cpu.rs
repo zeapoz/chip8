5eb7d5cf601b9b45162dc6bb4f21569b1e8a9e41
zeapoz <zeapo@pm.me>
2022-01-30 19:16:41 +0100

    Execute instruction changes for cpu

--- src/cpu.rs
+++ src/cpu.rs
@@ -20,7 +20,19 @@ impl Cpu {
     pub fn execute_instruction(&self, memory: &Memory) {
         let hi = memory.read_byte(self.pc);
         let lo = memory.read_byte(self.pc + 1);
+        let instruction: u16 = (hi as u16) << 8 | (lo as u16);
         
-        println!("0x{:X?}{:X?}", hi, lo);
+        println!("\nRead instruction: 0x{:X} from hi: 0x{:X} and lo: 0x{:X}", instruction, hi, lo);
+
+        let nnn = instruction & 0xFFF;
+        let n = instruction & 0xF;
+        let x = hi & 0xF;
+        let y = (lo & 0xF0) >> 4;
+        println!("nnn: 0x{:X}, n: 0x{:X}, x: 0x{:X}, y: 0x{:X}", nnn, n, x, y);
+
+        // TODO match instructions
+        match (instruction & 0xF000) >> 12 {
+            _ => panic!("Instruction not found for 0x{:X}", instruction),
+        }
     }
 }
